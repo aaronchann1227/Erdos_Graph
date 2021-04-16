403e3db6278b7cc90d6c4f94c70b3ccb02a5cbca
Jerry Wu <yihanwu4@illinois.edu>
2021-04-16 03:42:53 -0500

    Finished a basic csv to string vector model

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
-#include "ErdosRead.cpp"
-using namespace std;
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <iterator>
 
 int main() {
-    readErdos();
+    std::ifstream text("ErdosClean.csv");
+	
+	std::stringstream strStream;
+	if (text.is_open()) {
+		strStream << text.rdbuf();
+	}
+
+    std::string authors = strStream.str();
+    
+
+    std::vector<std::string> out;
+    std::stringstream ss(authors);
+    while(ss.good()) {
+      std::string substr;
+      std::getline(ss, substr, ','); //get first string delimited by comma
+      out.push_back(substr);
+    }
+    std::vector<std::string> result;
+    for (unsigned int i = 0; i < out.size(); i+=2) {
+        result.push_back(out[i] + " " + out[i+1]);
+    }
+    for (std::vector<std::string>::const_iterator i = result.begin(); i != result.end(); ++i)
+        std::cout << *i << "\n";
     return 0;
 }
