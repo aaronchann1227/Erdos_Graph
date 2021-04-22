9746d19fef806f8cc150b60d1a8f1beae12180e0
Jerry Wu <yihanwu4@illinois.edu>
2021-04-22 15:30:35 -0500

    Finished parsing the Publications.csv into unordered_map

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 #include <sstream>
 #include <iostream>
 #include <iomanip>
-
+#include <unordered_map>
 #include "ErdosRead.hpp"
 
 using namespace std;
@@ -22,7 +22,58 @@ int main() {
     
 
 
-       std::ifstream text("Erdos.csv");
+    // std::ifstream text("Erdos.csv");
+	
+	// std::stringstream strStream;
+	// if (text.is_open()) {
+	// 	strStream << text.rdbuf();
+	// }
+
+    // std::string authors = strStream.str();
+
+    // std::vector<std::string> out;
+    // std::string delimiter = "&";
+
+    // size_t pos = 0;
+    // std::string token;
+    // while ((pos = authors.find(delimiter)) != std::string::npos) {
+    //     token = authors.substr(0, pos);
+    //     out.push_back(token);
+    //     authors.erase(0, pos + delimiter.length());
+    // }
+
+    
+    // unsigned con = 0;
+    // out[0].erase(0,1);
+
+    // // out[out.size() - 1].erase(out[out.size() - 1].length() - 2,out[out.size() - 1].length() - 1);
+    // // for (std::vector<std::string>::const_iterator i = out.begin(); i != out.end(); ++i) {
+    // //     con++;
+    // //     std::cout << *i << "\n";
+    // //     if (con > 100) {break;}
+    // // }
+    // // size_t size = out.size();
+    // // std::cout << size << "";
+    // // std::cout << out[out.size() - 1];
+
+
+    // out.push_back("end123");
+
+    // std::vector< std::vector<std::string> > erdosVec;
+    // erdosVec.resize(513, vector<string> (0));
+
+    // unsigned count = 0;
+    // for (std::vector<std::string>::const_iterator i = out.begin(); i != out.end(); ++i) {
+    //     while (*i != "end123") {
+    //         erdosVec[count].push_back(*i);
+    //         ++i;
+    //     }
+    //     count++;
+    // }
+
+    // return erdosVec;
+
+    std::ifstream text("Publications.csv");
 	
 	std::stringstream strStream;
 	if (text.is_open()) {
@@ -32,7 +83,7 @@ int main() {
     std::string authors = strStream.str();
 
     std::vector<std::string> out;
-    std::string delimiter = "&";
+    std::string delimiter = ":";
 
     size_t pos = 0;
     std::string token;
@@ -42,39 +93,13 @@ int main() {
         authors.erase(0, pos + delimiter.length());
     }
 
-    
-    unsigned con = 0;
-    out[0].erase(0,1);
-
-    // out[out.size() - 1].erase(out[out.size() - 1].length() - 2,out[out.size() - 1].length() - 1);
-    // for (std::vector<std::string>::const_iterator i = out.begin(); i != out.end(); ++i) {
-    //     con++;
-    //     std::cout << *i << "\n";
-    //     if (con > 100) {break;}
-    // }
-    // size_t size = out.size();
-    // std::cout << size << "";
-    // std::cout << out[out.size() - 1];
-
-
-    out.push_back("end123");
-
-    std::vector< std::vector<std::string> > erdosVec;
-    erdosVec.resize(513, vector<string> (0));
-
-    unsigned count = 0;
-    for (std::vector<std::string>::const_iterator i = out.begin(); i != out.end(); ++i) {
-        while (*i != "end123") {
-            erdosVec[count].push_back(*i);
-            ++i;
-        }
-        count++;
+ 
+    std::unordered_map<std::string , unsigned int> authorToPaper;
+    for (size_t i = 0; i < out.size(); i += 2) {
+        authorToPaper[out[i]] = std::stoi(out[i + 1]); 
+        cout << out[i] << "\n";
+        cout << authorToPaper[out[i]] << "\n";
     }
-
-    return erdosVec;
-
-
-    
     return 0;
 }
 
