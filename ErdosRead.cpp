b63afd18de2550920464903da0aee58fedb20227
Man Chong Chan <mcchan3@siebl-0224-38.ews.illinois.edu>
2021-05-05 16:41:54 -0500

    adjust file structure

--- ErdosRead.cpp
+++ ErdosRead.cpp
@@ -21,41 +21,46 @@ std::vector<std::vector<std::string>> readErdos() {
 		strStream << text.rdbuf();
 	}
 
-    std::string authors = strStream.str();
+  std::string authors = strStream.str();
 
-    std::vector<std::string> out;
-    std::string delimiter = "&";
+  std::vector<std::string> out;
+  std::string delimiter = "&";
 
-    size_t pos = 0;
-    std::string token;
-    while ((pos = authors.find(delimiter)) != std::string::npos) {
-        token = authors.substr(0, pos);
-        out.push_back(token);
-        authors.erase(0, pos + delimiter.length());
-    }
+  size_t pos = 0;
+  std::string token;
+  while ((pos = authors.find(delimiter)) != std::string::npos) {
+      token = authors.substr(0, pos);
+      out.push_back(token);
+      authors.erase(0, pos + delimiter.length());
+  }
 
-    out[0].erase(0,1);
+  //out[0].erase(0,1);
 
-    out.push_back("end123");
+  out.push_back("end123");
 
-    std::vector< std::vector<std::string> > erdosVec;
-    erdosVec.resize(512, std::vector<std::string> (0));
+  std::vector< std::vector<std::string> > erdosVec;
+  erdosVec.resize(512, std::vector<std::string> (0));
 
-    unsigned count = 0;
-    unsigned counter2 = 0;
-
-    for (std::vector<std::string>::const_iterator i = out.begin(); i != out.end(); ++i) {
-        
-        while (*i != "end123") {
+  unsigned count = 0;
+  for (std::vector<std::string>::const_iterator i = out.begin(); i != out.end(); ++i) {
+      while (*i != "end123") {
             erdosVec[count].push_back(*i);
             ++i;
             if ( i == out.end() ) {break;}
         }
         if (i == out.end() ) {break;}
+        //std::cout << count << "\n";
         count++;
-    }
-
-    return erdosVec;
+        if (count == 512) {
+            //cout << "line 61";
+            break;
+        }
+        //std::cout << "line 60" << "\n";
+  }
+  
+  //getting a bug that the last line of file is not being parsed -- manual insert
+  // erdosVec[513] = vector<string>{string("ZIV, ABRAHAM"),string("Aharoni, Merav"),string("Amdursky, Vardy"),string("Asaf, Sigal"),string("Fournier, Laurent"),string("Gal, Shmuel"), string("GINZBURG, ABRAHAM"),string("Maharik, Ron"),string("Nehama, Ilan"),string("Nikulshin, Ilya"),string("Rubinstein, Reuven Y.")};
+  return erdosVec;
 }
 
 std::unordered_map<std::string, unsigned int> createAuthorToPaper() {
@@ -73,14 +78,24 @@ std::unordered_map<std::string, unsigned int> createAuthorToPaper() {
 
   size_t pos = 0;
   std::string token;
+
   while ((pos = authors.find(delimiter)) != std::string::npos) {
       token = authors.substr(0, pos);
       out.push_back(token);
       authors.erase(0, pos + delimiter.length());
   }
+
   std::unordered_map<std::string , unsigned int> authorToPaper;
+
+  unsigned count = 0;
   for (size_t i = 0; i < out.size(); i += 2) {
       authorToPaper[out[i]] = std::stoi(out[i + 1]); 
+      //std::cout << "createAuthorToPaper" << count;
+      count++;
+      if (count == 202) {
+            //cout << "break at 202";
+            break;
+      }
   }
   return authorToPaper;
 }
--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,5 +1,7 @@
 #include "../catch/catch.hpp"
 #include "../Graph.h"
+#include "../ErdosRead.hpp"
+#include "../ErdosRead.cpp"
 //#include "../Traversal.h"
 
 #include <iostream>
@@ -16,91 +18,6 @@
 #include <vector>
 #include <unordered_map>
 using namespace std;
-
-std::vector<std::vector<std::string>> readErdos() {
-  std::ifstream text("Erdos.csv");
-	
-	std::stringstream strStream;
-	if (text.is_open()) {
-		strStream << text.rdbuf();
-	}
-
-  std::string authors = strStream.str();
-
-  std::vector<std::string> out;
-  std::string delimiter = "&";
-
-  size_t pos = 0;
-  std::string token;
-  while ((pos = authors.find(delimiter)) != std::string::npos) {
-      token = authors.substr(0, pos);
-      out.push_back(token);
-      authors.erase(0, pos + delimiter.length());
-  }
-
-  //out[0].erase(0,1);
-
-  out.push_back("end123");
-
-  std::vector< std::vector<std::string> > erdosVec;
-  erdosVec.resize(512, std::vector<std::string> (0));
-
-  unsigned count = 0;
-  for (std::vector<std::string>::const_iterator i = out.begin(); i != out.end(); ++i) {
-      while (*i != "end123") {
-            erdosVec[count].push_back(*i);
-            ++i;
-            if ( i == out.end() ) {break;}
-        }
-        if (i == out.end() ) {break;}
-        //std::cout << count << "\n";
-        count++;
-        if (count == 512) {
-            //cout << "line 61";
-            break;
-        }
-        //std::cout << "line 60" << "\n";
-  }
-
-  return erdosVec;
-}
-
-std::unordered_map<std::string, unsigned int> createAuthorToPaper() {
-  std::ifstream text("Publications.csv");
-	
-	std::stringstream strStream;
-	if (text.is_open()) {
-		strStream << text.rdbuf();
-	}
-
-  std::string authors = strStream.str();
-
-  std::vector<std::string> out;
-  std::string delimiter = ":";
-
-  size_t pos = 0;
-  std::string token;
-
-  while ((pos = authors.find(delimiter)) != std::string::npos) {
-      token = authors.substr(0, pos);
-      out.push_back(token);
-      authors.erase(0, pos + delimiter.length());
-  }
-
-  std::unordered_map<std::string , unsigned int> authorToPaper;
-
-  unsigned count = 0;
-  for (size_t i = 0; i < out.size(); i += 2) {
-      authorToPaper[out[i]] = std::stoi(out[i + 1]); 
-      //std::cout << "createAuthorToPaper" << count;
-      count++;
-      if (count == 202) {
-            //cout << "break at 202";
-            break;
-      }
-  }
-  return authorToPaper;
-}
 
 Graph makeGraph() {
   std::cout << " enter makeGraph()" << std::endl;
