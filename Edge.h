c2e96c5d63061fbf5609f2e43c4a509feef5f1a2
Man Chong Chan <mcchan3@siebl-0224-33.ews.illinois.edu>
2021-05-05 21:38:30 -0500

    added comments

--- Edge.h
+++ Edge.h
@@ -5,12 +5,18 @@
 
 class Edge {
     public:
+        /**
+        * Constructor of an Edge
+        */
         Edge(std::string setV1, std::string setV2, double setWeight) {
             vertex1 = setV1;
             vertex2 = setV2;
             weight = setWeight;
         }
         
+        // the two ends of the Edge
         std::string vertex1, vertex2;
+
+        // the weight of the Edge
         double weight;
 };
--- Vertex.h
+++ Vertex.h
@@ -5,24 +5,48 @@
 
 class Vertex {
     public:
+        /**
+        * Constructor for Vertex
+        */
         Vertex(std::string& author) : author(author) {}
 
+        /**
+        * function that return the Mathematician of the Vertex
+        */
         std::string getAuthor() { return author; }
 
+        /**
+        * function to set the Mathematician of the Vertex
+        */
         void setAuthor(std::string setAuthor) {this->author = setAuthor;}
 
+        /**
+        * function to add an edge to the Vertex
+        */
         void addEdge(Edge* edge) {edges.push_back(edge);}
 
+        /**
+        * function that return the edge list of the Vertex
+        */
         std::vector<Edge*> getEdge(){
             return edges;
         }
 
+        /**
+        * function that ID of the Vertex
+        */
         unsigned int getID(){
             return id;
         }
         
+        /**
+        * function that set the ID of the Vertex
+        */
         void setID(unsigned int id) {this->id = id;}
 
+        /**
+        * function that takes in an Mathematician and determines if that Mathematician is a neighbor of the Vertex.
+        */
         bool isNeighbor(std::string author) {
             bool result = false;
             for (unsigned i = 0; i < edges.size(); ++i) {
@@ -34,6 +58,9 @@ class Vertex {
             return result;
         }
 
+        /**
+        * function that return a specific edge to a neighbor give the name of the Mathematician
+        */
         Edge* getEdge(std::string author) {
             for (unsigned i = 0; i < edges.size(); ++i) {
                 if ((edges[i]->vertex1 == author) ||  (edges[i]->vertex2 == author)) {
@@ -44,9 +71,12 @@ class Vertex {
         }
 
     private:
+        // the name of the Mathematician
         std::string author;
 
+        // the list of all the edges
         std::vector<Edge*> edges;
 
+        // the ID of the vertex
         unsigned int id;
 };
