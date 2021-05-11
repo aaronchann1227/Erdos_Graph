d24fd4e2069222112d2457144329c78a8125db80
Man Chong Chan <mcchan3@siebl-0224-06.ews.illinois.edu>
2021-05-11 00:07:55 -0500

    linker eror

--- main.cpp
+++ main.cpp
@@ -15,21 +15,30 @@
 #include "ErdosRead.cpp"
 #include "Graph.h"
 #include "Animation.h"
+#include "Traversal.h"
 using std::cout;
 using std::endl;
+using namespace std;
 
 int main() {
     
     Graph graph(readErdos(), createAuthorToPaper());
 
-    
-    PNG animation = graph.BCVisualize();
+    Vertex* Erdos = graph.getRoot();
+    Traversal Traversal(graph, Erdos);
+    int count = -1;
+    while (!Traversal.T_done()){
+        count += 1;
+        ++Traversal;
+    }
 
-    /* for (unsigned int i = 0; i < 100; i++) {
-        //cout << animation[i].first << endl;
-        //cout << animation[i].second << endl;
-    } */
-    animation.writeToFile("ErdosPNG.png");
+    // print out the first 10 names that BFS traversed.
+    cout << "The first 10 Mathematicians that BFS traverses: ";
+    Traversal.printBFSvisited(10);
+
+    
+    // PNG animation = graph.BCVisualize();
+    // animation.writeToFile("ErdosPNG.png");
 
     return 0;
 }
